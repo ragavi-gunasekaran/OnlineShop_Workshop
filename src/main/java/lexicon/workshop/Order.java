33bfb0d84d21278cfef35b89ef5de20be34ae32a
Ragavi Gunasekaran <ragavigunasekaran@gmail.com>
2026-03-19 09:44:36 +0100

    added Order class contructor and getter setter method

--- src/main/java/lexicon/workshop/Order.java
+++ src/main/java/lexicon/workshop/Order.java
@@ -1,4 +1,36 @@
 package lexicon.workshop;
 
 public class Order {
+
+    private int orderId;
+    private double totalOrderPrice;
+
+    public Order(int orderId, double totalOrderPrice) {
+        this.orderId = orderId;
+        this.totalOrderPrice = totalOrderPrice;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public double getTotalOrderPrice() {
+        return totalOrderPrice;
+    }
+
+    public void setTotalOrderPrice(double totalOrderPrice) {
+        this.totalOrderPrice = totalOrderPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "orderId=" + orderId +
+                ", totalOrderPrice=" + totalOrderPrice +
+                '}';
+    }
 }
