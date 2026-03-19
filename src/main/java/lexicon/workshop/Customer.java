6c4191ac6064b430cd00e20025c35e48db851582
Ragavi Gunasekaran <ragavigunasekaran@gmail.com>
2026-03-19 09:42:19 +0100

    Added constructor , getter & setter methods for customer class

--- src/main/java/lexicon/workshop/Customer.java
+++ src/main/java/lexicon/workshop/Customer.java
@@ -1,4 +1,47 @@
 package lexicon.workshop;
 
 public class Customer {
+
+    private int customerId;
+    private String customerName;
+    private String customerEmail;
+
+    public Customer(int customerId, String customerName, String customerEmail) {
+        this.customerId = customerId;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "customerId=" + customerId +
+                ", customerName='" + customerName + '\'' +
+                ", customerEmail='" + customerEmail + '\'' +
+                '}';
+    }
 }
